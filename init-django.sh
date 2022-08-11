696d536e01c22559717a4f797b7af5709371f8df
Victor Catalán <donbungle@gmail.com>
2022-08-11 01:36:38 -0400

    script de instalacion app

--- init-django.sh
+++ init-django.sh
@@ -1,7 +1,13 @@
-virtualenv venv
-source ./venv/bin/activate
-python3 -m pip install -r requirements.txt
-python3 -m django startproject proy_dir
+# virtualenv venv
+# source ./venv/bin/activate
+# python3 -m pip install -r requirements.txt
+# python3 -m django startproject proy_dir
+# cd proy_dir
+# python3 -m django startapp base_app
+# python3 -m django migrate
+
+
+python -m django startproject proy_dir
 cd proy_dir
-python3 -m django startapp base_app
-python3 -m django migrate
+python -m django startapp base_app
+python -m django migrate
